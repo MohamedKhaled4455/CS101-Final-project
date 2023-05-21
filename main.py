e2eab3900d78f7593b19e879882d8f2ccbd71856
Mohamed Khaled <88774059+MohamedKhaled4455@users.noreply.github.com>
2023-05-21 22:23:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ total_price=0
 
 # Function to read inputs from a file
 def read_inputs_from_file(products):
+    # Open the file in read mode
     file = open(products, "r")
+    # Read all lines from the file and store them in a list
     inputs= file.readlines()
+    # Return the list of inputs
     return inputs
 
 
@@ -23,21 +26,28 @@ def print_to_screen_and_file(text, screen, file):
 
 # Function to create a product database
 def create_product_database():
+    # Create an empty dictionary to store the product database
     product_db = {}
+    # Iterate over each line from the input file
     for line in read_inputs_from_file("products.txt"):
+        # Split the line by comma to extract individual values
         product_code, product_name, product_price, product_stock = line.split(",")
+        # Create a dictionary for each product and store its information
         product_db[product_code] = {
             "name": product_name,
             "price": float(product_price),
             "stock": int(product_stock),
         }
+    # Return the product database dictionary
     return product_db
 
 
 # Function to display the product menu
 def display_menu(product_db):
     print("Product Code\tProduct Name\tPrice\tStock")
+    # Iterate over the product database dictionary
     for code, values in product_db.items(): #loop on keys and its values
+        # Print the product details
         print(f"{code}\t\t\t\t {values['name']}\t\t\t{values['price']}\t\t{values['stock']}")
     print("* Press 2 to process order\n")
 
@@ -46,22 +56,23 @@ def process_order(product_db):
     global code,name,unit_price,quantity,total_price
 
     code = input("Enter product code: ")
+    # Update the name and unit_price global variables based on the entered code
     name = product_db[code]["name"] # updating global variables after taking the code
     unit_price = product_db[code]["price"] # updating global variables after taking the code
-
+    #check if the code is found in database
     if code not in product_db:
         print("Invalid product code")
         return
-
+    #check if there is enough amount products in stock to process with
     quantity = int(input("Enter quantity: "))
     if quantity > product_db[code]["stock"]:
         print("Insufficient stock")
         return
-
+    #printing the total price of product
     total_price = quantity * product_db[code]["price"]
     product_db[code]["stock"] -= quantity
 
-    # Update the quantity in the file for the corresponding code
+    # Update the quantity in the file (product.txt) for the corresponding code
     lines = read_inputs_from_file("products.txt")
     for i in range(len(lines)):
         product_line = lines[i].split(",")
@@ -69,7 +80,7 @@ def process_order(product_db):
             product_line[3] = str(product_db[code]["stock"])
             lines[i] = ",".join(product_line)+ "\n"
             break
-
+    # Open the file in write mode to update the quantity
     file = open("products.txt", "w")
     for line in lines:
         file.write(line)
@@ -80,6 +91,7 @@ def process_order(product_db):
 
 # Function to generate a receipt
 def generate_receipt():
+    #calling the global variables that are defined before
     global code,name,unit_price,quantity,total_price
     print("Receipt:")
     print("Code\t\tProduct Name\t\tUnit Price\t\tQuantity")
@@ -91,6 +103,7 @@ def generate_receipt():
 
 # Function to main menu
 def main_menu():
+    # the list to choose from
     while True:
         print("1. Display menu")
         print("2. Process order")
@@ -108,5 +121,6 @@ def main_menu():
 
 
 if __name__ == "__main__":
+    # calling the database to be used in other functions
     product_db = create_product_database()
     main_menu()
